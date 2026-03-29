18c05f878d00651074b3655e570fa0c3d85d7c13
Kevi5002 <jmejia3@northpark.edu>
2026-03-29 16:59:31 -0500

    JS added

--- js/script.js
+++ js/script.js
@@ -1,1 +1,6 @@
 console.log("script.js loaded");
+
+const gifContainer = document.getElementById("gif-container");
+const fetchBtn = document.querySelector("#fetch-gif-btn");
+const searchInput = document.querySelector("#search-input");
+
