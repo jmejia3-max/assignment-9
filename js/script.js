74219af8a7083ceee5e93c06e4d00ce9c5cfcffd
Kevi5002 <jmejia3@northpark.edu>
2026-03-29 17:29:43 -0500

    Updated html, css, and JS
    No api key yet but added search. Doesn't work

--- js/script.js
+++ js/script.js
@@ -4,3 +4,33 @@ const gifContainer = document.getElementById("gif-container");
 const fetchBtn = document.querySelector("#fetch-gif-btn");
 const searchInput = document.querySelector("#search-input");
 
+const API_KEY = "";
+
+fetchBtn.addEventListener("click", async () => {
+    
+    // Clear previous GIFs
+    gifContainer.innerHTML = "";
+
+    // Get search term
+    const searchTerm = searchInput.value;
+
+    try {
+        // Fetch data from Giphy
+        const response = await fetch(`${API_KEY}&q=${searchTerm}&limit=12`);
+        const data = await response.json();
+
+        const images = data.data;
+
+        // Loop through GIFs
+        images.forEach(gif => {
+            const imageUrl = gif.images.fixed_height.url;
+
+            gifContainer.innerHTML += `
+                <img src="${imageUrl}" class="col-3 mb-3">
+            `;
+        });
+
+    } catch (error) {
+        console.error("Error fetching GIFs:", error);
+    }
+});
